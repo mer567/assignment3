c3c3ffcd858699a6ffd9c9f44ed2423763aef73d
Maya Rotmensch <mer567@nyu.com>
2014-09-25 13:00:42 -0400

    initial commit

---
+++ mer567/my_package/hello_world.py
@@ -0,0 +1,16 @@
+'''
+Created on Sep 24, 2014
+
+@author: Maya Rotmensch (mer567)
+
+'''
+
+def hello_world():
+    print "Hello World!"
+
+#
+# Python hello world
+#
+if __name__ == "__main__":
+    import sys
+    hello_world()
